036ffc18972643e391b0faf78fb98ee3675fc214
C. Reyes <cvreyes@hotmail.com>
2026-04-20 11:47:25 +0800

    No. 187

--- blog/models.py
+++ blog/models.py
@@ -26,7 +26,8 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=256)
     excerpt = models.CharField(max_length=1024)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
+    # image_name = models.CharField(max_length=100)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
